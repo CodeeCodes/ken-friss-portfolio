78200f1f9d1108c89729dbddf16a1de32459b8a2
Codee Palmer <codeejpalmer@gmail.com>
2020-02-17 21:06:42 -0800

    Added Kens logo gif, worked on mobile styling for Gallery section

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,25 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Logo from "../assets/ken-logo.gif";
 
 export default function header() {
   return (
     <div className="header">
       <Link to="/">
-        {" "}
-        <h1 className="header__list-heading">Ken Friss</h1>
+        <img src={Logo} alt="logo" className="header__logo" />
       </Link>
       <ul className="header__list">
         <li className="header__list-item">
-          <Link to="/about">About</Link>
+          <Link to="/about">
+            <h6 className="header__heading-link">About</h6>
+          </Link>
         </li>
         <li className="header__list-item">
-          <Link to="/reels">Reels</Link>
+          <Link to="/reels">
+            <h6 className="header__heading-link">Reels</h6>
+          </Link>
         </li>
         <li className="header__list-item">
-          <Link to="/gallery">Gallery</Link>
+          <Link to="/gallery">
+            <h6 className="header__heading-link">Gallery</h6>
+          </Link>
         </li>
         <li className="header__list-item">
-          <Link to="/contact">Contact</Link>
+          <Link to="/contact">
+            <h6 className="header__heading-link">Contact</h6>
+          </Link>
         </li>
       </ul>
     </div>
